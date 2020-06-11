cc4159df3ecad69e9c8da9d7ed5952aac7819b21
omar <contact@asweb.com>
2020-06-11 11:57:06 -0500

    sa

--- aprendiendo-angular/src/app/externo/externo.component.ts
+++ aprendiendo-angular/src/app/externo/externo.component.ts
@@ -10,13 +10,16 @@ import {PeticionesService} from '../services/peticiones.service'
 export class ExternoComponent implements OnInit {
   public user:any
   public userId:any
+  public fecha:any
   constructor(
     private _peticionesService:PeticionesService
   ) {
     this.userId=1
+
   }
 
   ngOnInit(): void {
+    this.fecha=new Date(2019,5,30)
     this.cargaUsuario()
   }
 
